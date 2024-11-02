9c17270a26eeb97c07e3c9b9a97e6521b7c047f7
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-11-02 20:35:08 +0100

    feat: replaced template files

---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+import User from '../schemas/User.js';
+
+export const register = async (req, res) => {};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
