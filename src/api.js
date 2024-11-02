e23422888e9e0f895d4f2866ef67057faae2f683
adrrf <adrf.dev@gmail.com>
2024-11-02 16:33:57 +0100

    chore: Upstream vitest changes

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+const swaggerDocument = YAML.load('./openapi.yaml');
+
+export default function () {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', (req, res) => {
+    res.send('API funcionando correctamente');
+  });
+
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+  return app;
+}
