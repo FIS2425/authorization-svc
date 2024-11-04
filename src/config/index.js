e820090ec5bc39622bd4a2feb6d5d75b26b9e765
antonio <antoniorodriguezruiz2002@gmail.com>
2024-11-04 19:46:49 +0100

    feat: added connection to dragonfly (redis)

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,39 @@
 import mongoose from 'mongoose';
 import api from '../api.js';
+import redis from 'redis';
 
 const MONGO_URI = process.env.MONGOURL;
 const PORT = process.env.PORT || 3001;
+
+const redisClient = redis.createClient({
+  host: process.env.DRAGONFLY_HOST,
+  port: process.env.DRAGONFLY_PORT,
+});
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log('Conexión con MongoDB OK');
+
+    redisClient.connect();
+
+    redisClient.on('connect', () => {
+      console.log('Connected to Redis...');
+    });
+
+    redisClient.on('error', (err) => {
+      console.error('Redis error:', err);
+    });
+
+    // Listen for end and close events
+    redisClient.on('end', () => {
+      console.log('Redis client disconnected');
+    });
+
+    // Optionally, handle reconnection logic
+    redisClient.on('reconnecting', () => {
+      console.log('Attempting to reconnect to Redis...');
+    });
 
     const app = api();
 
@@ -18,3 +44,5 @@ mongoose
   .catch((error) => {
     console.error('Error de conexión con MongoDB:', error.message);
   });
+
+export default redisClient;
