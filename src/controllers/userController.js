00736da48802abe0c31c1770a49f5cdbb208d5a0
antonio <antoniorodriguezruiz2002@gmail.com>
2024-11-04 19:47:19 +0100

    feat: switched AuthToken for cache

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../schemas/User.js';
-import AuthToken from '../schemas/AuthToken.js';
 import jwt from 'jsonwebtoken';
+import redisClient from '../config/index.js';
 
 export const register = async (req, res) => { };
 
@@ -10,24 +10,29 @@ export const login = async (req, res) => {
   try {
     const user = await User.findOne({ name });
     if (await user.comparePassword(password)) {
-      const authToken = await jwt.sign({
-        userId: user._id.toString(),
-        roles: user.roles,
-      }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRATION || '1h',
-      });
-      const refreshToken = await jwt.sign({
-        userId: user._id.toString(),
-      }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_REFRESH_EXPIRATION || '7d',
-      });
+      const authToken = await jwt.sign(
+        {
+          userId: user._id.toString(),
+          roles: user.roles,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRATION || 3600,
+        },
+      );
+      const refreshToken = await jwt.sign(
+        {
+          userId: user._id.toString(),
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_REFRESH_EXPIRATION || '7d',
+        },
+      );
 
-      // We save the token to the database, so that in cases of emergy we can revoke it
-      await new AuthToken({
-        token: authToken,
-        userId: user,
-        roles: user.roles,
-      }).save();
+      // We save the token to the cache, so that in cases of emergy we can revoke it
+      redisClient.set(authToken, user._id.toString(), { EX: parseInt(process.env.JWT_EXPIRATION) || 3600 });
+      redisClient.set(refreshToken, user._id.toString(), { EX: parseInt(process.env.JWT_REFRESH_EXPIRATION) || 3600 });
 
       res.cookie('token', authToken, { httpOnly: true });
       res.cookie('refreshToken', refreshToken, { httpOnly: true });
@@ -36,7 +41,8 @@ export const login = async (req, res) => {
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
-  } catch (_) {
+  } catch (error) {
+    console.log(error);
     res.status(401).json({ message: 'Invalid credentials' });
   }
 };
@@ -45,15 +51,13 @@ export const logout = async (req, res) => {
   res.clearCookie('token');
   res.clearCookie('refreshToken');
 
-  const token = req.cookies.token;
+  const authToken = req.cookies.token;
 
-  if (!token) {
+  if (!authToken) {
     res.status(401).json({ message: 'Not logged in' });
   } else {
     try {
-      await AuthToken.deleteOne({
-        token: token
-      });
+      redisClient.del(authToken);
       res.status(200).json({ message: 'Logout successful' });
     } catch (error) {
       // Here token has probably expired. To the user it's the same as if it was deleted
