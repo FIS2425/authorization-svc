d4367d6814fbeaaaa8b384d43717de1947bd59ea
antonio <antoniorodriguezruiz2002@gmail.com>
2024-11-08 19:56:48 +0100

    test: added integration tests for create user

--- tests/integration/controllers/userController.test.js
+++ tests/integration/controllers/userController.test.js
@@ -4,6 +4,7 @@ import * as db from '../../setup/database';
 import { request } from '../../setup/setup';
 import User from '../../../src/schemas/User.js';
 import { redisClient } from '../../../src/config/redis.js';
+import jwt from 'jsonwebtoken';
 
 const sampleUser = new User({
   _id: uuidv4(),
@@ -12,9 +13,37 @@ const sampleUser = new User({
   roles: ['patient'],
 });
 
+const clinicAdmin = new User({
+  _id: uuidv4(),
+  email: 'clinicAdmin@test.com',
+  password: 'password',
+  roles: ['clinicadmin'],
+});
+
+const sampleUserToken = jwt.sign({ userId: sampleUser._id, roles: sampleUser.roles }, process.env.VITE_JWT_SECRET);
+const clinicAdminToken = jwt.sign({ userId: clinicAdmin._id, roles: clinicAdmin.roles }, process.env.VITE_JWT_SECRET);
+
 beforeAll(async () => {
   await db.clearDatabase();
   await sampleUser.save();
+  await clinicAdmin.save();
+
+  redisClient.set(sampleUserToken, sampleUser._id.toString(), async () => { });
+  redisClient.set(clinicAdminToken, clinicAdmin._id.toString(), async () => { });
+
+  // We mock `exists` because `redis-mock` is not compatible with `redis 4`, so we change the behavior of the method
+  vi.spyOn(redisClient, 'exists').mockImplementation((key) => {
+    return new Promise((resolve, reject) => {
+      redisClient.get(key, (err, value) => {
+        if (err) {
+          reject(false);
+        } else {
+          console.log('Token exists', value);
+          resolve(value ? 1 : 0);
+        }
+      });
+    });
+  });
 });
 
 afterAll(async () => {
@@ -92,5 +121,97 @@ describe('User Controller Integration Tests', () => {
       expect(response.status).toBe(200);
       expect(response.body.message).toBe('Logout successful');
     });
+  });
+
+  describe('createUser', () => {
+    it('should create a user successfully', async () => {
+      const mockUser = {
+        email: 'email@test.com',
+        password: 'password',
+        roles: ['patient'],
+      };
+
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${clinicAdminToken}`])
+        .send({
+          email: 'email@test.com',
+          password: 'password',
+          roles: ['patient'],
+        });
+
+      expect(response.status).toBe(201);
+      expect(response.body.email).toBe(mockUser.email);
+      expect(response.body.roles).toEqual(mockUser.roles);
+      expect(response.body).not.toHaveProperty('password');
+    });
+
+    it('should return 400 when required email and password not sent', async () => {
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${clinicAdminToken}`])
+        .send({});
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        email: 'Email is required',
+        password: 'Password is required',
+      });
+    });
+
+    it('should return 400 when required email and password empty', async () => {
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${clinicAdminToken}`])
+        .send({ email: '', password: '' });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        email: 'Email is required',
+        password: 'Password is required',
+      });
+    });
+
+    it('should return 400 if user already exists', async () => {
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${clinicAdminToken}`])
+        .send({ email: 'testuser@test.com', password: 'password' });
+
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBe('A user with that email already exists.');
+    });
+
+    it('should return 400 on failed user attr validation', async () => {
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${clinicAdminToken}`])
+        .send({
+          email: 'email2.com',
+          password: 'password',
+          roles: ['user'],
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.messages).toEqual([
+        'Invalid email address',
+        '`user` is not a valid enum value for path `roles.0`.'
+      ]);
+    });
+
+    it('should return 403 if a non clinicadmin tries to create an user', async () => {
+      const response = await request
+        .post('/users')
+        .set('Cookie', [`token=${sampleUserToken}`])
+        .send({
+          email: 'email2.com',
+          password: 'password',
+          roles: ['patient'],
+        });
+
+      expect(response.status).toBe(403);
+      expect(response.body.message).toEqual('Unauthorized');
+    });
+
   });
 });
