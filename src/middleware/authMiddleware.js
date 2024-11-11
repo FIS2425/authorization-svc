f0c9d4b8ceb0d4f540d14d7649a0a9f9e33190f4
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-11-11 23:43:38 +0100

    refactor: removed unused middlewares

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,21 +2,6 @@ import logger from '../config/logger.js';
 import User from '../schemas/User.js';
 import Role from '../schemas/Role.js';
 
-export const checkRoles = (...roles) => {
-  return async (req, res, next) => {
-    if (!roles.every((role) => req.roles.includes(role))) {
-      logger.warn('Forbidden', {
-        method: req.method,
-        url: req.originalUrl,
-        userId: req.userId,
-        ip: req.ip,
-      });
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-    next();
-  };
-};
-
 export const userExists = async (req, res, next) => {
   const user = await User.findById(req.params.id);
   if (!user) {
@@ -29,24 +14,6 @@ export const userExists = async (req, res, next) => {
     return res.status(404).json({ message: 'User not found' });
   }
   req.onUser = user;
-  next();
-};
-
-export const hasAccessToUser = async (req, res, next) => {
-  const hasAccess =
-    req.roles.includes('admin') ||
-    req.roles.includes('clinicadmin') ||
-    req.roles.includes('doctor') ||
-    req.params.id === req.userId.toString();
-  if (!hasAccess) {
-    logger.warn('Forbidden', {
-      method: req.method,
-      url: req.originalUrl,
-      userId: req.userId,
-      ip: req.ip,
-    });
-    return res.status(403).json({ message: 'Forbidden' });
-  }
   next();
 };
 
@@ -159,14 +126,14 @@ export const authorizeRequest = (method) => {
 
       next();
     } catch (error) {
-      logger.error('Server error', {
+      logger.error('Internal server error', {
         method: req.method,
         url: req.originalUrl,
         userId: req.userId,
         ip: req.ip,
         error: error.message,
       });
-      res.status(500).json({ message: 'Server error' });
+      res.status(500).json({ message: 'Internal server error' });
     }
   };
 };
