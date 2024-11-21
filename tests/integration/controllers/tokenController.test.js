83b0d9173ace2f0ade72f5901ea30b108191687b
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-11-22 00:32:43 +0100

    test added refresh token tests

--- tests/integration/controllers/tokenController.test.js
+++ tests/integration/controllers/tokenController.test.js
@@ -31,86 +31,202 @@ afterAll(async () => {
   await db.clearDatabase();
 });
 
-describe('Validation Middleware', () => {
+describe('Token Controller', () => {
   let token;
+  
+  describe('validate', () => {
+    beforeAll(async () => {
+      token = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET
+      );
+      redisClient.set(token, sampleUser._id.toString());
+    });
 
-  beforeAll(async () => {
-    token = jwt.sign(
-      { userId: sampleUser._id, roles: sampleUser.roles },
-      process.env.VITE_JWT_SECRET
-    );
-    redisClient.set(token, sampleUser._id.toString());
-  });
+    it('should validate the token successfully', async () => {
+      const res = await request.get('/token/validate').set('Cookie', `token=${token}`);
 
-  it('should validate the token successfully', async () => {
-    const res = await request.get('/token/validate').set('Cookie', `token=${token}`);
+      expect(res.status).toBe(200);
+    });
 
-    expect(res.status).toBe(200);
-  });
+    it('should return 401 with no token provided', async () => {
+      const res = await request.get('/token/validate').set('Cookie', '');
 
-  it('should return 401 with no token provided', async () => {
-    const res = await request.get('/token/validate').set('Cookie', '');
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('No token provided');
+    });
 
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe('No token provided');
-  });
+    it('should return 401 with token expired', async () => {
+      const expiredToken = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET,
+        { expiresIn: 0 }
+      );
 
-  it('should return 401 with token expired', async () => {
-    const expiredToken = jwt.sign(
-      { userId: sampleUser._id, roles: sampleUser.roles },
-      process.env.VITE_JWT_SECRET,
-      { expiresIn: 0 }
-    );
+      const res = await request
+        .get('/token/validate')
+        .set('Cookie', `token=${expiredToken}`);
 
-    const res = await request
-      .get('/token/validate')
-      .set('Cookie', `token=${expiredToken}`);
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Token expired');
+    });
 
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe('Token expired');
-  });
-
-  it('should return 401 with token expired in dragonfly', async () => {
+    it('should return 401 with token expired in dragonfly', async () => {
     // From here on out redis will not need to be accessed, so we can safely delete the token
-    await redisClient.del(token);
+      await redisClient.del(token);
 
-    const expiredToken = jwt.sign(
-      { userId: sampleUser._id, roles: sampleUser.roles },
-      process.env.VITE_JWT_SECRET,
-      { expiresIn: 0 }
-    );
+      const expiredToken = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET,
+        { expiresIn: 0 }
+      );
 
-    const res = await request
-      .get('/token/validate')
-      .set('Cookie', `token=${expiredToken}`);
+      const res = await request
+        .get('/token/validate')
+        .set('Cookie', `token=${expiredToken}`);
 
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe('Token expired');
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Token expired');
+    });
+
+    it('should return 401 if user is not found', async () => {
+      const nonExistentUserId = uuidv4();
+      const invalidToken = jwt.sign(
+        { userId: nonExistentUserId, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET
+      );
+
+      const res = await request
+        .get('/token/validate')
+        .set('Cookie', `token=${invalidToken}`);
+
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('User not found');
+    });
+
+    it('should return 401 with invalid token (badly signed)', async () => {
+      const invalidToken = 'invalid.token.value';
+
+      const res = await request
+        .get('/token/validate')
+        .set('Cookie', `token=${invalidToken}`);
+
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Invalid token');
+    });
   });
 
-  it('should return 401 if user is not found', async () => {
-    const nonExistentUserId = uuidv4();
-    const invalidToken = jwt.sign(
-      { userId: nonExistentUserId, roles: sampleUser.roles },
-      process.env.VITE_JWT_SECRET
-    );
+  describe('refresh', () => {
+    let validToken, validRefreshToken;
+  
+    beforeAll(async () => {
+      validToken = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET
+      );
+      validRefreshToken = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET
+      );
+  
+      redisClient.set(validToken, sampleUser._id.toString());
+      redisClient.set(validRefreshToken, sampleUser._id.toString());
+      redisClient.sadd(`user_tokens:${sampleUser._id}`, validToken);
+      redisClient.sadd(`user_tokens:${sampleUser._id}`, validRefreshToken);
+    });
+  
+    afterAll(async () => {
+      await redisClient.del(validToken);
+      await redisClient.del(validRefreshToken);
+      await redisClient.srem(`user_tokens:${sampleUser._id}`, validToken);
+      await redisClient.srem(`user_tokens:${sampleUser._id}`, validRefreshToken);
+    });
+  
+    it('should refresh tokens successfully', async () => {
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}; refreshToken=${validRefreshToken}`);
+  
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe('Tokens refreshed');
+  
+      const newToken = res.headers['set-cookie'].find((cookie) =>
+        cookie.startsWith('token=')
+      );
+      const newRefreshToken = res.headers['set-cookie'].find((cookie) =>
+        cookie.startsWith('refreshToken=')
+      );
+  
+      expect(newToken).toBeDefined();
+      expect(newRefreshToken).toBeDefined();
+    });
+  
+    it('should return 401 if no refreshToken is provided', async () => {
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}`);
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('No token provided');
+    });
+  
+    it('should return 401 if refreshToken is expired', async () => {
+      const expiredRefreshToken = jwt.sign(
+        { userId: sampleUser._id, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET,
+        { expiresIn: 0 }
+      );
+  
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}; refreshToken=${expiredRefreshToken}`);
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Token expired');
+    });
+  
+    it('should return 401 if refreshToken is invalid', async () => {
+      const invalidRefreshToken = 'invalid.token.value';
+  
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}; refreshToken=${invalidRefreshToken}`);
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Invalid token');
+    });
+  
+    it('should return 401 if user does not exist', async () => {
+      const nonExistentUserId = uuidv4();
+      const invalidRefreshToken = jwt.sign(
+        { userId: nonExistentUserId, roles: sampleUser.roles },
+        process.env.VITE_JWT_SECRET
+      );
+  
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}; refreshToken=${invalidRefreshToken}`);
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('User not found');
+    });
 
-    const res = await request
-      .get('/token/validate')
-      .set('Cookie', `token=${invalidToken}`);
+    it('should return 401 if user does not have a token', async () => {
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', 'token=invalidToken; refreshToken=invalidRefreshToken');
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Invalid token');
+    });
 
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe('User not found');
-  });
-
-  it('should return 401 with invalid token (badly signed)', async () => {
-    const invalidToken = 'invalid.token.value';
-
-    const res = await request
-      .get('/token/validate')
-      .set('Cookie', `token=${invalidToken}`);
-
-    expect(res.status).toBe(401);
-    expect(res.body.message).toBe('Invalid token');
-  });
+    it('should return 401 if user does not have a refresh token', async () => {
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${validToken}; refreshToken=invalidRefreshToken`);
+  
+      expect(res.status).toBe(401);
+      expect(res.body.message).toBe('Invalid token');
+    });
+  });  
 });
--- tests/unit/controllers/tokenController.test.js
+++ tests/unit/controllers/tokenController.test.js
@@ -8,28 +8,89 @@ afterEach(() => {
   vi.resetAllMocks();
 });
 
-describe('Validation Controller', () => {
-  it('should return 200', async () => {
+describe('Token Controller', () => {
+  describe('validate token', () => {
+    it('should return 200', async () => {
     // We test happy path as the middleware is already tested
-    const mockUser = {
-      _id: 'userId',
-      email: 'testuser@mail.com',
-      password: 'password',
-      roles: ['user'],
-      comparePassword: vi.fn().mockResolvedValue(true),
-    };
-    const mockToken = 'validToken';
-    const mockDecoded = { userId: 'user123', roles: ['user'] };
+      const mockUser = {
+        _id: 'userId',
+        email: 'testuser@mail.com',
+        password: 'password',
+        roles: ['user'],
+        comparePassword: vi.fn().mockResolvedValue(true),
+      };
+      const mockToken = 'validToken';
+      const mockDecoded = { userId: 'user123', roles: ['user'] };
 
-    vi.spyOn(jwt, 'verify').mockReturnValueOnce(mockDecoded);
-    vi.spyOn(User, 'findById').mockResolvedValue(mockUser);
-    vi.spyOn(redisClient, 'exists').mockResolvedValue(true);
+      vi.spyOn(jwt, 'verify').mockReturnValueOnce(mockDecoded);
+      vi.spyOn(User, 'findById').mockResolvedValue(mockUser);
+      vi.spyOn(redisClient, 'exists').mockResolvedValue(true);
 
-    const res = await request
-      .get('/token/validate')
-      .set('Cookie', `token=${mockToken}`);
+      const res = await request
+        .get('/token/validate')
+        .set('Cookie', `token=${mockToken}`);
 
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe('Token is valid');
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe('Token is valid');
+    });
+  });
+
+  describe('refresh token', () => {
+    it('should return 200', async () => {
+      const mockUser = {
+        _id: 'userId',
+        email: 'testuser@mail.com',
+        password: 'password',
+        roles: ['user'],
+        comparePassword: vi.fn().mockResolvedValue(true),
+      };
+      const mockToken = 'validToken';
+      const mockRefresh = 'validRefreshToken';
+      const mockDecoded = { userId: 'user123', roles: ['user'] };
+
+      vi.spyOn(jwt, 'verify').mockReturnValueOnce(mockDecoded);
+      vi.spyOn(User, 'findById').mockResolvedValue(mockUser);
+      vi.spyOn(redisClient, 'exists').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'del').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'sismember').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'srem').mockResolvedValue(true);
+
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${mockToken}; refreshToken=${mockRefresh}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe('Tokens refreshed');
+    });
+    it('should return 500', async () => {
+      const mockUser = {
+        _id: 'userId',
+        email: 'testuser@mail.com',
+        password: 'password',
+        roles: ['user'],
+        comparePassword: vi.fn().mockResolvedValue(true),
+      };
+      const mockToken = 'validToken';
+      const mockRefresh = 'validRefreshToken';
+      const mockDecoded = { userId: 'user123', roles: ['user'] };
+
+      vi.spyOn(jwt, 'verify').mockReturnValueOnce(mockDecoded);
+      vi.spyOn(User, 'findById').mockResolvedValue(mockUser);
+      vi.spyOn(redisClient, 'exists').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'del').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'sismember').mockResolvedValue(true);
+      vi.spyOn(redisClient, 'srem').mockResolvedValue(true);
+
+      vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error('JWT signing error');
+      });
+
+      const res = await request
+        .get('/token/refresh')
+        .set('Cookie', `token=${mockToken}; refreshToken=${mockRefresh}`);
+
+      expect(res.status).toBe(500);
+      expect(res.body.message).toBe('Internal server error');
+    });
   });
 });
